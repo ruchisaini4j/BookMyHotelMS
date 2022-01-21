0374a2160ab76c296747d39b439f41e74ebfa25c
Ruchi Saini <ruchisaini4j@gmail.com>
2022-01-21 21:00:57 +0530

    Registration MicroService

---
+++ 03-MicroServices/03-01-RegistrationService/src/main/java/com/bookMyHotel/controllers/RegistrationApiController.java
@@ -0,0 +1,46 @@
+package com.bookMyHotel.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bookMyHotel.model.Registration;
+import com.bookMyHotel.services.RegistrationService;
+
+@RestController
+public class RegistrationApiController {
+	
+	@Autowired
+	RegistrationService registrationService;
+	
+	
+	@PostMapping(path="/api/registrations",produces="application/xml",consumes="application/xml")
+	public  String addCustomerRegistration(@RequestBody Registration registration) {
+		return registrationService.addCustomerRegistration(registration);
+	}
+	
+	@GetMapping(path="/api/registrations",produces="application/xml")
+	public String checkLoginDetails(@RequestParam String loginId, @RequestParam String password)
+	{
+		return registrationService.checkLoginDetails(loginId,password);
+	}
+
+
+	
+	@GetMapping(path="/api/registrations/{registrationId}",produces="application/xml")
+	public  List<String> getRegistrationDetails(@PathVariable("registrationId") String registrationId) {
+		
+		return registrationService.getRegistrationDetails(registrationId);
+		
+	}
+	
+	
+	
+
+}
